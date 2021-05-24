d6b458b40b2c5586a5d22baa366d725ce4a1e1be
Tom Giddings <tom.giddings@gmail.com>
2021-05-24 13:35:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useState } from 'react';
+
 
 function App() {
+  const [entryUrl, setEntryUrl] = useState('');
+  const [responseUrl, setResponseUrl] = useState('');
+
+  const handleSubmit = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url: entryUrl })
+    };
+    const response = await fetch('http://localhost:3001/url', options);
+    const data = await response.json();
+    console.log(await data)
+    setResponseUrl(window.location.href + data.key)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography component="h1" variant="h3">Shorten URL</Typography>
+          <FormControl
+            fullWidth
+          >
+            <TextField
+              id="standard-basic"
+              label="Enter url to shorten"
+              onChange={(e) => setEntryUrl(e.target.value)}
+            />
+            <Button onClick={handleSubmit}>Shorten</Button>
+          </FormControl>
+          {responseUrl.length > 0 &&
+            <Box>
+              <Typography component="h2" variant="h5">Result:</Typography>
+              <Typography>{responseUrl}</Typography>
+            </Box>
+          }
+        </Box>
+    </Container>
   );
 }
 
